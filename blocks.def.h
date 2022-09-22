1189d199485162f810b2e11b8486e2df4fba63a9
Zagyarakushi <6381630-zagyarakushi@users.noreply.gitlab.com>
2022-09-22 17:46:18 +0100

    Fixed the signal. You need to add 35 to get the right signal instead of 34

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,9 @@
 static const Block blocks[] = {
         /*Icon*/        /*Command*/             /*Update Interval*/     /*Update Signal*/
-        {" ",   "cat /tmp/recordingicon 2>/dev/null",   0,                      9},
-        /*{"  ", "~/.local/bin/statusbar/blevel",      0,                      11},*/
-        {" ",   "~/.local/bin/statusbar/volume",        0,                      10},
-        /*{" ", "~/.local/bin/statusbar/battery",       5,                      0},*/
+        {" ",   "cat /tmp/recordingicon 2>/dev/null",   0,                      8},
+        {"  ", "~/.local/bin/statusbar/blevel",      0,                      10},
+        {" ",   "~/.local/bin/statusbar/volume",        0,                      9},
+        {" ", "~/.local/bin/statusbar/battery",       5,                      0},
         {" ",   "~/.local/bin/statusbar/clock",         60,                     0},
         {"  ", "~/.local/bin/statusbar/cpu",           5,                      0},
         {" ",   "~/.local/bin/statusbar/internet",      5,                      0},
